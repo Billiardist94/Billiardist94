5a782d66aaf244dc73c0339fb3329c121b163b26
ADMIN <billiardist@bk.ru>
2019-06-03 17:43:13 +0300

    commited

---
+++ Operation.py
@@ -0,0 +1,18 @@
+x = input('Введите выражение: ')
+# print(x)
+# pl = '+'
+# min = '-'
+# mult = '*'
+# div = '/'
+# for i in x:
+#     if i == pl:
+#         y = int(x[0]) + int(x[2])
+#     elif i == min:
+#         y = int(x[0]) - int(x[2])
+#     elif i == mult:
+#         y = int(x[0]) * int(x[2])
+#     elif i == div:
+#         y = int(x[0]) / int(x[2])
+#
+# print(int(y))
+print(eval(x))
