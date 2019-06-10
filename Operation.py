60fc57cf95a8df8e92f825e0dc3eb29b5f8a0150
ADMIN <billiardist@bk.ru>
2019-06-10 21:57:20 +0300

    Generator2

--- Operation.py
+++ Operation.py
@@ -16,3 +16,32 @@ x = input('Введите выражение: ')
 #
 # print(int(y))
 print(eval(x))
+s = input('Введите выражение: ')
+
+tokens = []
+while True:
+    for i in range(len(s)):
+        if s[i] not in {'+', '-', '*', '/'}:
+            continue
+        tokens += float(s[:i]), s[i]
+        s = s[i+1:]
+        break
+    else:
+        tokens += float(s),
+        break
+
+i = 1
+while i < len(tokens) - 1:
+    token = tokens[i]
+    if token == '*':
+        tokens[i - 1] *= tokens[i + 1]
+        del tokens[i:i+2]
+    elif token == '/':
+        ...  # ваш код для /
+    else:
+        i += 1
+
+...  # ваш код для + и -
+
+assert len(tokens) == 1
+print(tokens[0])
--- _Generator2.py
+++ _Generator2.py
@@ -1,12 +1,6 @@
 def chain(it1, it2, it3):
+    return list(it1) + list(it2) + list(it3)
 
-    for x in range(it1):
-        print(x)
-    print()
-    for y in range(*it2):
-        print(y)
-    print()
-    for z in range(*it3):
-        print(z)
 
-chain(3, (1, 4), (2, 5))
+for x in chain(range(3), range(1, 4), range(2, 5)):
+    print(x, end=', ')
